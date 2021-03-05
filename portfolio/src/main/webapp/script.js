7c549c5d1738c2275bfe24b00a2194061ff313fc
Kendall Hester <khester@sps-program.com>
2021-03-05 06:57:59 +0000

    Add fetch cmd to frontend

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,8 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function getPacerText() {
+    const res = await fetch('/pacer-text');
+    document.getElementById('pacer-text').innerText = await res.text();
+}
